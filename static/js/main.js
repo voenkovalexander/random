b42af013f88b39cc9c419eb39f4e3796eeb3d4db
voenkovalexander <156917408+voenkovalexander@users.noreply.github.com>
2024-01-22 21:47:22 +0300

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+document.getElementById("button").addEventListener("click", randomFunc);
+
+function randomFunc(event){
+    event.preventDefault();
+
+    var par1 = document.getElementById("par1").value;
+    var par2 = document.getElementById("par2").value;
+
+    var res = Math.round(Math.random() * (par2 - par1) + par1);
+
+    document.getElementById("output").innerHTML = res;
+}
+
+
+document.getElementById("dark-button").addEventListener("click", darkThemeFunc);
+
+function darkThemeFunc(event) {
+    event.preventDefault();
+
+    document.getElementById('html').classList.toggle("dark-theme");
+}
+
